17413e9e7571215be8e390a663f4eadf51498c49
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2022-04-24 14:45:17 -0700

    Initiate the app with useEffect

--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer({ api }) {
+
+  const [sushiList, setSushiList] = useState([])
+
+  useEffect(() => {
+    fetch(api)
+      .then(res => res.json())
+      .then(data => setSushiList(data))
+  }, [])
+
+  function renderSushis() {
+    return sushiList.map(sushi => {
+      return <Sushi sushi={sushi}/>
+    })
+  }
+
   return (
     <div className="belt">
       {/* Render Sushi components here! */}
